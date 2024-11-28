50003b5e0a61ae43a7cd4c7857e3f4a95e764988
goo3oo <goo3oo0918@gmail.com>
2024-11-28 17:40:14 +0900

    fix: Align paragraphs

--- src/com/example/kiosk/level4/Cart.java
+++ src/com/example/kiosk/level4/Cart.java
@@ -1,14 +1,15 @@
 package com.example.kiosk.level4;
+
 import java.util.*;
 
 public class Cart {
     public List<MenuItem> cart = new ArrayList<>();
 
-    public void addItem(MenuItem menuItem){
+    public void addItem(MenuItem menuItem) {
         cart.add(menuItem);
     }
 
-    public void clearCart(){
+    public void clearCart() {
         cart.clear();
     }
 }
